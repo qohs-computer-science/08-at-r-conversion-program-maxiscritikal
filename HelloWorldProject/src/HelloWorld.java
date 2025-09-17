08864004f7839f767d919ea570c89ce75fae7f60
maxiscritikal <101420657+maxiscritikal@users.noreply.github.com>
2025-09-17 17:39:18 +0000

    2nc Iteration 9/17/25

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -15,39 +15,49 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int x = 0;
 		String binary = "";
 		int decimal = 0;
+
+		int x = 0;
 		while (x < 1)
 		{
-			System.out.println("Do you want decimal to binary converstion or the other way around? Enter '1' for decimal to binary and '2' for the other oprion.");
+			System.out.println("Welcome!  You have the following options: /n 1. Binary to Decimal /n 2. Decimal to Binary");
 			int input = in.nextInt();
 
 			if (input == 1)
 			{
-				System.out.println("Okay now enter a decimal number from 0 - 127:");
+				x++;
+				
 			}
 
 			else if (input == 2)
 			{
+				x++;
 				
 			}
 
 			else
 			{
-
+				System.out.println("Enter 1 or 2 based on the options given.");
 			}
 		}
+		
+		System.out.println("Okay now enter a decimal number from 0 - 127:");
+		int tempDec = in.nextInt();
+		decimal = tempDec;
+
 
 		in.close();
 	}
 
 	public static String decToBin() {
-		
+		String temp = "hello";
+		return temp;
 	}
 
 	public static int binToDec() {
-		
+		int temp = 5;
+		return temp;
 	}
 
 }
